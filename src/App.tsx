c6f230db7e52b5541c30550be554687f717891df
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-09-12 19:01:05 +0300

    clone

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList/TodoList';
+import { filterTodos } from './helper/filterTodos';
+import { useLocaleStorage } from './helper/useLocalStorage';
+
+export const App: React.FC = () => {
+  const [todos, setTodos] = useLocaleStorage('todos');
+  const amountCompletedTodos = useMemo(() => {
+    return todos.reduce((acc, curr) => {
+      return curr.completed ? acc + 1 : acc;
+    }, 0);
+  }, [todos]);
+  const { pathname: location } = useLocation();
+  const isCompletedTodo = useMemo(() => {
+    return !todos.some(todo => todo.completed === false);
+  }, [todos]);
+
+  const visibleTodos = filterTodos(todos, location);
+
+  return (
+    <div className="todoapp">
+      <Header
+        setTodos={setTodos}
+      />
+
+      {!todos.length || (
+        <>
+          <TodoList
+            isCompletedTodo={isCompletedTodo}
+            todos={visibleTodos}
+            setTodos={setTodos}
+          />
+          <Footer
+            todos={todos}
+            setTodos={setTodos}
+            amountCompletedTodos={amountCompletedTodos}
+          />
+        </>
+      )}
+    </div>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import { App } from './App';
+
+import './styles/index.css';
+import './styles/todo-list.css';
+import './styles/filters.css';
+
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById('root'),
+);
